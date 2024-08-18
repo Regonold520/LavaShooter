991b09d5fbf870cd72e89b3437fbdd94b5794d83
Regonold <regonold520@gmail.com>
2024-08-18 21:04:19 +0100

    Created new project

--- GODOT Lava/BulletsManager.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var bullet_scene = preload("res://bullet.tscn")
-
-func _on_player_shoot(pos):
-	var bullet = bullet_scene.instantiate()
-	$"..".add_child(bullet)
-	bullet.position = pos
